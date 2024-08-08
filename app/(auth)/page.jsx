0a13da299053f2414dea5c944debf66240e088c9
Paweł <pawelzarebski7@wp.pl>
2024-08-08 20:17:53 +0200

    no register

--- app/(auth)/page.jsx
+++ app/(auth)/page.jsx
@@ -59,19 +59,19 @@ const Login = () => {
                 <div className="text-center 2xl:mb-10 mb-4">
                   <h4 className="font-medium">Zaloguj</h4>
                   <div className="text-slate-500 text-base">
-                    Zaloguj się do panelu A.I. i skomunikuj się z naszym asystentem
+                    Zaloguj się do panelu A.I. BEZ REJESTRACJI i skomunikuj się z naszym asystentem
                   </div>
                 </div>
                 <LoginForm />
-                <div className="relative border-b-[#9AA2AF] border-opacity-[16%] border-b pt-6">
+                {/* <div className="relative border-b-[#9AA2AF] border-opacity-[16%] border-b pt-6">
                   <div className="absolute inline-block bg-white dark:bg-slate-800 dark:text-slate-400 left-1/2 top-1/2 transform -translate-x-1/2 px-4 min-w-max text-sm text-slate-500 font-normal">
                     Kontynuuj z
                   </div>
                 </div>
                 <div className="max-w-[242px] mx-auto mt-8 w-full">
                   <Social />
-                </div>
-                <div className="md:max-w-[345px] mx-auto font-normal text-slate-500 dark:text-slate-400 mt-12 uppercase text-sm">
+                </div> */}
+                {/* <div className="md:max-w-[345px] mx-auto font-normal text-slate-500 dark:text-slate-400 mt-12 uppercase text-sm">
                   Nie masz konta{" "}
                   <Link
                     href="/register"
@@ -79,7 +79,7 @@ const Login = () => {
                   >
                     Zarejestruj się
                   </Link>
-                </div>
+                </div> */}
               </div>
               <div className="auth-footer text-center">
                 Copyright 2024, NovaTech All Rights Reserved.
